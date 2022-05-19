b142c66e9bc62dc7c66a50c5d37198eae7b42ef6
Seb - physical <svidal1@free.fr>
2022-05-19 18:39:00 +0200

    improve typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
-declare module "node-confmanager" {
+/// <reference types="node" />
 
-	type tType = "array" | "boolean" | "color" | "email" | "float" | "ipv4" | "ipv6" | "integer" | "object" | "string" | "url";
+declare module "node-confmanager" {
 
 	import Container = require("node-containerpattern");
 
@@ -22,23 +22,6 @@ declare module "node-confmanager" {
 		public fileExists(): Promise<boolean>;
 		public load(): Promise<void>;
 		public save(): Promise<void>;
-
-		// rewrite to force proper return
-		public clear(): ConfManager;
-		public clearData(): ConfManager;
-		public clearDocumentations(): ConfManager;
-		public clearLimits(): ConfManager;
-		public clearMinsMaxs(): ConfManager;
-		public clearRegexs(): ConfManager;
-		public clearSkeletons(): ConfManager;
-		public delete(key: string): ConfManager;
-		public document(key: string, value: string): ConfManager;
-		public limit(key: string, limit: Array<any>): ConfManager;
-		public min(key: string, min: number): ConfManager;
-		public max(key: string, max: number): ConfManager;
-		public regex(key: string, regex: RegExp): ConfManager;
-		public set(key: string, value: any): ConfManager;
-		public skeleton(key: string, value: tType): ConfManager;
 
 	}
 
--- test/typescript/compilation.ts
+++ test/typescript/compilation.ts
@@ -1,31 +1,44 @@
 /// <reference path="../../lib/index.d.ts" />
 
-import ConfManager = require("../../lib/main.js");
-import { join } from "path";
+"use strict";
 
-const Conf = new ConfManager(join(__dirname, "conf.json"));
+// deps
+
+	// natives
+	import { join } from "path";
+
+	// locals
+	import ConfManager = require("../../lib/main.js");
+
+// consts
+
+	const Conf: ConfManager = new ConfManager(join(__dirname, "conf.json"));
+
+// module
 
 Conf
-  .skeleton("debug", "boolean").shortcut("debug", "d")
-  .shortcut("usr.login", "ul")
-  .shortcut("usr.password", "up");
+	.skeleton("debug", "boolean").shortcut("debug", "d")
+	.shortcut("usr.login", "ul")
+	.shortcut("usr.password", "up");
 
-Conf.fileExists().then((exists: boolean) => {
+Conf.fileExists().then((exists: boolean): Promise<void> => {
 
-  return Conf.set("usr", { login : "login", pwd : "pwd" })
-      .set("debug", false)
-      .set("prod", "n") // = false
-      .save();
+	console.log(exists);
 
-}).then(() => {
+	return Conf.set("usr", { login : "login", pwd : "pwd" })
+			.set("debug", false)
+			.set("prod", "n") // = false
+			.save();
 
-  return Conf.load();
+}).then((): Promise<void> => {
 
-}).then(() => {
+	return Conf.load();
 
-    console.log(Conf.get("debug"));
-    console.log(Conf.get("usr.login"));
+}).then((): void => {
 
-}).catch((err: Error) => {
-  console.log(err);
+	console.log(Conf.get("debug"));
+	console.log(Conf.get("usr.login"));
+
+}).catch((err: Error): void => {
+	console.log(err);
 });
